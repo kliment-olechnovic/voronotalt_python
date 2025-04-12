a7f6fd132db37b6110219dc0a68006c5a97a54d2
klap <klap@klap-machine>
2025-04-12 23:35:40 +0200

    First commit.

---
+++ run_swig_and_compile_binary.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd "$(dirname $0)"
+
+rm -rf "./voronotalt_python_wrap.cxx" "./_voronotalt_python.so" "./voronotalt_python.py" "./__pycache__"
+
+swig -python -c++ voronotalt_python.i
+
+g++ -fPIC -shared -Ofast -march=native -fopenmp -I ./cpp/ voronotalt_python_wrap.cxx -o _voronotalt_python.so $(python3-config --includes)
+
